1a74b17b970fe1c560101d1d16fa297f18197c59
Sergey <sergey.morenets@yandex.ua>
2019-09-28 13:02:19 +0300

    Task #5

--- rest-task4/src/main/java/it/discovery/model/Book.java
+++ rest-task4/src/main/java/it/discovery/model/Book.java
@@ -1,17 +1,24 @@
 package it.discovery.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Getter
 @Setter
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Book {
 	private int id;
 
-	private String author;
+    @JsonProperty("title")
+    @XmlElement(name = "title2")
+    private String author;
 
 	private String name;
 	
