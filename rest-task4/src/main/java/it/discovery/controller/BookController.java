537e1380845c61ccfd0d1d36afcaa5c410ce5a63
Sergey <sergey.morenets@yandex.ua>
2019-09-28 14:34:09 +0300

    Task #6

--- rest-task4/src/main/java/it/discovery/controller/BookController.java
+++ rest-task4/src/main/java/it/discovery/controller/BookController.java
@@ -1,23 +1,40 @@
 package it.discovery.controller;
 
 import it.discovery.model.Book;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import it.discovery.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("book")
+@RequiredArgsConstructor
 public class BookController {
 
-    @GetMapping(value = "sample", produces = {
-            MediaType.APPLICATION_JSON_UTF8_VALUE,
-            MediaType.APPLICATION_XML_VALUE
-    })
-    public Book sampleBook() {
-        Book book = new Book();
-        book.setAuthor("Unknown");
-        book.setName("Spring REST");
-        return book;
+    private final BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> findAll() {
+        return bookRepository.findAll();
     }
+
+    @GetMapping("{id}")
+    public Book findById(@PathVariable int id) {
+        return bookRepository.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@RequestBody Book book) {
+        bookRepository.save(book);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        bookRepository.delete(id);
+    }
+
 }
