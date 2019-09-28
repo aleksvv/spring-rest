e93bd84a4873c01de54ebc30c00fb4f7b83e43a8
Sergey <sergey.morenets@yandex.ua>
2019-09-28 12:01:10 +0300

    Task #3

---
+++ rest-task2/src/main/java/it/discovery/UtilController.java
@@ -0,0 +1,23 @@
+package it.discovery;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@RestController
+@RequestMapping("current")
+public class UtilController {
+
+    @RequestMapping("time")
+    public String currentTime() {
+        return LocalTime.now().toString();
+    }
+
+    @RequestMapping("date")
+    public String currentDate() {
+        return LocalDate.now().toString();
+    }
+}
