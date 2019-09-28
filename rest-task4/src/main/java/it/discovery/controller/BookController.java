b951ac33cf412e085644ac0c50d85ecc31576613
Sergey <sergey.morenets@yandex.ua>
2019-09-28 12:42:56 +0300

    Task #4. Adjust response content-type

--- rest-task4/src/main/java/it/discovery/controller/BookController.java
+++ rest-task4/src/main/java/it/discovery/controller/BookController.java
@@ -1,6 +1,7 @@
 package it.discovery.controller;
 
 import it.discovery.model.Book;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("book")
 public class BookController {
 
-    @GetMapping("sample")
+    @GetMapping(value = "sample", produces = {
+            MediaType.APPLICATION_JSON_UTF8_VALUE,
+            MediaType.APPLICATION_XML_VALUE
+    })
     public Book sampleBook() {
         Book book = new Book();
         book.setAuthor("Unknown");
--- rest-task4/src/main/java/it/discovery/model/Book.java
+++ rest-task4/src/main/java/it/discovery/model/Book.java
@@ -3,8 +3,11 @@ package it.discovery.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 @Getter
 @Setter
+@XmlRootElement
 public class Book {
 	private int id;
 
