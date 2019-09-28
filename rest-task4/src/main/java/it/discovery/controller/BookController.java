3e7c6f0bde4401e5f296fbc733ea9e67dfa993e8
Sergey <sergey.morenets@yandex.ua>
2019-09-28 12:27:36 +0300

    Task #4. Add simple REST-service for books

--- rest-task4/src/main/java/it/discovery/controller/BookController.java
+++ rest-task4/src/main/java/it/discovery/controller/BookController.java
@@ -1,11 +1,19 @@
 package it.discovery.controller;
 
+import it.discovery.model.Book;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/book")
-public class BookController {	
+@RequestMapping("book")
+public class BookController {
 
-
+    @GetMapping("sample")
+    public Book sampleBook() {
+        Book book = new Book();
+        book.setAuthor("Unknown");
+        book.setName("Spring REST");
+        return book;
+    }
 }
