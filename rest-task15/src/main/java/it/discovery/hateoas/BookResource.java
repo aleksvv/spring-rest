df45c4cbed1fb5aef1156dc4e37d1dbe85a06710
Sergey <sergey.morenets@yandex.ua>
2019-09-29 17:18:31 +0300

    Task #18

--- rest-task15/src/main/java/it/discovery/hateoas/BookResource.java
+++ rest-task15/src/main/java/it/discovery/hateoas/BookResource.java
@@ -1,16 +1,22 @@
 package it.discovery.hateoas;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 import it.discovery.controller.BookController;
 import it.discovery.model.Book;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.hateoas.ResourceSupport;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Getter
+@Setter
 public class BookResource extends ResourceSupport {
+    private final static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
     private String name;
 
     private String author;
@@ -23,11 +29,7 @@ public class BookResource extends ResourceSupport {
     private boolean rented;
 
     public BookResource(Book book) {
-        name = book.getName();
-        author = book.getAuthor();
-        bookId = book.getId();
-        year = book.getYear();
-        rented = book.isRented();
+        mapper.map(book, this);
         add(linkTo(methodOn(BookController.class).findById(bookId)).withSelfRel());
         if (!rented) {
             add(linkTo(methodOn(BookController.class).rent(bookId)).withRel("rent"));
